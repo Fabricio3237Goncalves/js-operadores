c44dc77aa0bf402dd544c7ed492f2d3d8cd1d0ff
Fabricio3237goncalves <fabriciogoncalvesoliveira3@gmail.com>
2024-12-12 11:59:06 -0300

    Conectores

---
+++ index.js
@@ -0,0 +1,54 @@
+//Operadores de atribuição
+
+    //recebe
+        let nome = "Fabricio";
+        let idade = "28";
+
+    // atribuidor incremental
+        nome += " Gonçalves";
+        idade += "1";
+        idade = idade + 1;
+
+    // atribuidor decremental
+        idade -= 5;
+
+// Operadores Aritmeticos
+    let adicao = 2 + 2;
+    let subtracao =  2 - 2;
+    let multiplicacao = 2 * 2;
+    let divisao = 2 / 2;
+    let restoDaDivisao = 3 % 2;
+
+// Concatenacao
+    let concatenacao = '2' + '2';
+
+// Operadotes de comparacao
+    let igual = 2 == 2; //true
+    let igualDenovo = 2 == 3 //false
+    let diferente = 2 != 2; //true
+    let diferenteDenovo = 2 != 3 //false
+    let igualEmTudo =  2 === 2; //true
+    let igualQuaseTudo = 2 === 3; //false
+    let diferenteEmTudo =  2 !== 2; //false
+    let diferenteQuaseTudo = 2 !== 3; //true
+    let maiorQ = 2 > 3; //false
+    let menorQ = 3 < 2; //false
+    let maiorQ2 = 3 > 2; //true
+    let menorQ2 = 2 < 3; //true
+    let maiorouigualQ = 2 >= 3; //false
+    let maiorouigualQ2 = 3 >= 2; //true
+    let menorouigualQ = 3 <= 2; //false
+    let menorouigualQ2 = 2 <= 3; //true
+
+// Operadores Logicos
+    let eLogico = 2 == 2 && 3 == 3; //true
+    let eLogicoFalso = 2 != 2 && 3 == 3; //false
+    let ouLogico = 2 == 2 || 3 == 3; //true
+    let ouLogicoTrueTambem = 2 == 2 || 3 != 3; //true
+    let ouLogicoFalse = 2 != 2 || 3 != 3; //fase
+
+    let duplaNegacao = !!0 //diz o valor boleano natural
+    
+    
+
+
